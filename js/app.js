3bd0635c94206012366bef95e5ee0e4557b66e0c
bxxhxx <Tom@New-Box-17-Edit.local>
2016-01-15 12:37:31 -0800

    integrate wikipedia data to KO model
    bring in the full star data list and sort them based on their clickable
    categories

--- js/app.js
+++ js/app.js
@@ -1,73 +1,11 @@
-//Make preliminary list for first trial and added two more category items
-var modelList = [{
-    firstName: 'Muhammad',
-    lastName: 'Ali',
-    category: 'Live performance',
-    address: '6801 Hollywood Blvd.'
-}, {
-    firstName: 'Eddy',
-    lastName: 'Arnold',
-    category: 'Radio',
-    address: '6775 Hollywood Blvd.'
-}, {
-    firstName: 'Jay',
-    lastName: 'Leno',
-    category: 'Television',
-    address: '6780 Hollywood Blvd.'
-}, {
-    firstName: 'Robert Z.',
-    lastName: 'Leonard',
-    category: 'Motion pictures',
-    address: '6370 Hollywood Blvd.'
-}, {
-    firstName: 'Mervyn',
-    lastName: 'LeRoy',
-    category: 'Motion pictures',
-    address: '1560 Vine Street'
-}, {
-    firstName: 'Jack',
-    lastName: 'Lescoulie',
-    category: 'Television',
-    address: '6500 Hollywood Blvd.'
-}, {
-    firstName: 'Joan',
-    lastName: 'Leslie',
-    category: 'Television',
-    address: '1560 Vine Street'
-}, {
-    firstName: 'Sol',
-    lastName: 'Lesser',
-    category: 'Motion pictures',
-    address: '6533 Hollywood Blvd.'
-}, {
-    firstName: 'Earl',
-    lastName: 'Lestz',
-    category: 'Motion pictures',
-    address: '6807 Hollywood Blvd.'
-}, {
-    firstName: 'Oscar',
-    lastName: 'Levant',
-    category: 'Recording',
-    address: '6728 Hollywood Blvd.'
-}];
-
-
-//Make Star prototype with four specific parameters
-var Star = function(starItem) {
-
-    this.firstName = ko.observable(starItem.firstName);
-    this.lastName = ko.observable(starItem.lastName);
-    this.category = ko.observable(starItem.category);
-    this.address = ko.observable(starItem.address);
+//Make Star prototype with three specific parameters
+var Star = function(starObject) {
+    this.fullName = ko.observable(starObject.fullName);
+    this.category = ko.observable(starObject.category);
+    this.address = ko.observable(starObject.address);
     //starts the page with items hidden (unless a category is clicked)
     this.show = ko.observable(false);
-
-    //Computes a full name using KO
-    this.fullName = ko.computed(function() {
-        return this.firstName() + " " + this.lastName();
-    }, this);
 };
-
 
 //Create knockout ViewModel function
 var ViewModel = function() {
@@ -87,11 +25,21 @@ var ViewModel = function() {
     //Create a KO array with all main five categories
     this.category = ko.observableArray(['Live performance', 'Motion pictures', 'Radio', 'Recording', 'Television']);
 
-
-    //Takes each object from data list and conversts into a Star object
-    //using our Star prototype and pushes it into walkOfFame observable array
-    modelList.forEach(function(starItem) {
-        self.walkOfFameList.push(new Star(starItem));
+    //Takes each object (starItem) from the completeStarDataModel and creates one or more
+    //star object based on how many categories the named star has. Then each of these new
+    //star objects are pushed into walkOfFameList.
+    completeStarDataModel.forEach(function(starItem) {
+        var starName = starItem.fullName;
+        for (var i = 0; i < starItem.category.length; i++) {
+            var starCategory = starItem.category[i];
+            var starAddress = starItem.address[i];
+            var starObject = {
+                fullName: starName,
+                category: starCategory,
+                address: starAddress
+            };
+            self.walkOfFameList.push(new Star(starObject));
+        }
     });
 
     //Make a method that allows us to tap in to google's geo code API
@@ -116,19 +64,20 @@ var ViewModel = function() {
                 }
             });
     };
+
+    //A new observable that captures whatever category is clicked and triggers
+    //a visible style change
+    this.currentCategory = ko.observable("");
     //compares data from category click to each walkOfFameList item
     // and shows ("true") matching items; hides ("false") un-matching items
     this.categorySelector = function(data) {
-            for (i = 0; i < self.walkOfFameList().length; i++) {
-                if (data === self.walkOfFameList()[i].category()) {
-                    self.walkOfFameList()[i].show(true);
-                } else {
-                    self.walkOfFameList()[i].show(false);
-                }
+        self.currentCategory(data);
+        for (i = 0; i < self.walkOfFameList().length; i++) {
+            if (data === self.walkOfFameList()[i].category()) {
+                self.walkOfFameList()[i].show(true);
+            } else {
+                self.walkOfFameList()[i].show(false);
             }
         }
+    }
 };
-
-
-//Activates KO on ViewModel
-ko.applyBindings(new ViewModel());
--- js/datalookup.js
+++ js/datalookup.js
@@ -2,25 +2,39 @@
 var wikiUrl = "https://en.wikipedia.org/w/api.php";
 //Make an array to store a complete list
 var completeStarDataModel = [];
+
+function isEmpty(obj) {
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop))
+            return false;
+    }
+    return true;
+
+}
 //This function sorts the data coming from Wikipedia (ajax request)
 var sortTheData = function(response) {
     //Using split to divide up the raw data from wikipedia in to info strings
     var rawDataArray = response.split("| [[");
-
     var clean1DataArray = [];
-    //This for loop will resturcture each data string into a more coherent data object
+    //This for loop will restructure each data string into a more coherent data object
     //It will skip item [0] which was header information
     for (var i = 1; i < rawDataArray.length; i++) {
         var newObject = {};
+
         //Using replace function to get rid off extraenous characters
         rawDataArray[i] = rawDataArray[i].replace("]]", "");
-        rawDataArray[i] = rawDataArray[i].replace("\n|-\n|", "||");
-        rawDataArray[i] = rawDataArray[i].replace("\n|-\n|", "||");
-        rawDataArray[i] = rawDataArray[i].replace("\n|-\n|", "||");
-        rawDataArray[i] = rawDataArray[i].replace("\n|-\n|", "||");
-        rawDataArray[i] = rawDataArray[i].replace("\n|-\n", "");
+        rawDataArray[i] = rawDataArray[i].replace(/\n\|-\n/g, "||");
+        rawDataArray[i] = rawDataArray[i].replace(/\|\|\|/g, "||");
         //Splits different category of information based on the double pipe
         newObjectSourceArray = rawDataArray[i].split("||");
+
+        newObjectSourceArray.pop();
+        for (var j = 0; j < newObjectSourceArray.length; j++) {
+            //trim the white space around data strings
+            newObjectSourceArray[j] = newObjectSourceArray[j].trim();
+            console.log(newObjectSourceArray[j]);
+        }
+
         //The following if/else if's directs the data into named properties
         //on the empty newObject
         if (newObjectSourceArray.length === 3) {
@@ -64,21 +78,14 @@ var sortTheData = function(response) {
             newObject.category.push(newObjectSourceArray[9]);
             newObject.address.push(newObjectSourceArray[10]);
         }
+
         //Pushing the new newObject sorted above into the completeStarDataModel
-        completeStarDataModel.push(newObject);
-
-        //var newObjectSourceArray = rawDataArray[i].split("||")
+        if (isEmpty(newObject) === false) {
+            completeStarDataModel.push(newObject);
+            //console.log(newObject);
+        }
     }
-
-    /*console.log("Here's the sorted array item 1: ");
-    console.log(rawDataArray[1]);
-    console.log("Here's the completeStarDataModel item 1 fullName: ");
-    console.log(completeStarDataModel[0].fullName);
-    console.log(completeStarDataModel[0].category);
-    console.log(completeStarDataModel[0].address);*/
-
-}
-
+};
 
 //This is ajax request with specific parameters requested by Wikipedia's API in
 //order to get the full information from the walk of fame list page
@@ -97,4 +104,6 @@ $.ajax({
     var wikiItems = response.query.pages["1310953"].revisions[0]["*"];
     //console.log(wikiItems);
     sortTheData(wikiItems);
+    //Activates KO on ViewModel
+    ko.applyBindings(new ViewModel());
 });
